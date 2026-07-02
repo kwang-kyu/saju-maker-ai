d94f61907b767326f2bdf53a347a68664a9064a7
kwang-kyu <kikwku23@gmail.com>
2026-07-03 04:23:20 +0900

    extract detail docx prepared data

--- src/v2/services/docx/docxDetailSections.ts
+++ src/v2/services/docx/docxDetailSections.ts
@@ -1,4 +1,5 @@
 ﻿import type { DocxSection } from "./detailDocxService";
+import { basicMapper } from "../basic/basicMapper";
 
 export function buildDetailDocxSections(
   sections: DocxSection[],
@@ -7,4 +8,26 @@ export function buildDetailDocxSections(
 
   return detailSections;
 }
+
+export function buildDetailDocxPreparedData(params: {
+  name: string;
+  birthDate: string;
+  birthTime: string;
+  gender: string;
+}) {
+  const { name, birthDate, birthTime, gender } = params;
+  const inputData = { name, birthDate, birthTime, gender };
+  const mappedBasic = basicMapper(inputData);
+
+  const sajuPersonalNote = `${name}님은 일간 기준으로 ${mappedBasic.summary}
+강하게 살아나는 기운은 ${mappedBasic.strongestElement}, 보완이 필요한 기운은 ${mappedBasic.weakestElement}입니다.
+이 흐름은 돈, 일, 관계, 건강, 사업 판단에 함께 반영됩니다.`;
+
+  return {
+    inputData,
+    mappedBasic,
+    sajuPersonalNote,
+  };
+}
+
 
